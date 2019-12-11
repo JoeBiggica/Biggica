ea4984795a35bfba9f2a5d962fa46e48405afc1a
Joe Biggica <joe.biggica@thomsonreuters.com>
2019-12-11 11:58:53 -0500

    Added static files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c72e7170307ecda1ca05";
+/******/ 	var hotCurrentHash = "7db99e162c4563e151d0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
